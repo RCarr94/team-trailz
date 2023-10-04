63fb75bfdcb4a919119f669b65ce31b61ee25e41
Ryan Carr <ryan.g.carr@hotmail.com>
2023-10-04 11:30:48 -0400

    Updated navbar, added home page folder

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+
+// Components
+import NavBar from '../../components/NavBar/NavBar';
+
+// Pages
+import HomePage from '../HomePage/HomePage';
+
+// Helpers
+import { getUser } from '../../utilities/services/users';
+
+import './App.css';
+
+export default function App() {
+  const [user, setUser] = useState(getUser());
+
+  return (
+    <main className="App">
+      <>
+        <NavBar user={user} setUser={setUser} />
+        <HomePage />
+      </>
+    </main>
+  );
+}

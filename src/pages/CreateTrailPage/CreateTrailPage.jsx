fe824d62ec5d8f970adc79c4c80f83557d751e0d
Ryan Carr <ryan.g.carr@hotmail.com>
2023-10-21 22:31:35 -0400

    fix component

--- src/pages/CreateTrailPage/CreateTrailPage.jsx
+++ src/pages/CreateTrailPage/CreateTrailPage.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react';
 import { create } from '../../utilities/api/trails';
-import { usePlacesWidget } from 'react-google-autocomplete';
 import { useNavigate } from 'react-router-dom';
 import './CreateTrailPage.css';
 
-// import GoogleSearchBar from '../../components/GoogleSearchBar/GoogleSearchBar';
+import GoogleSearchBar from '../../components/GoogleSearchBar/GoogleSearchBar';
 
 
 
@@ -22,33 +21,10 @@ export default function CreateTrailPage({ trailItems, setTrailItems }) {
   const [location, setLocation] = useState('');
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
-  const [inputLocation, setInputLocation] = useState(location);
   const navigate = useNavigate();
 
   
   const { trailName, difficulty, length, description, error } = formData;
-
-
-  const { ref: materialRef } = usePlacesWidget({
-    apiKey: 'AIzaSyDVRXAWPrWRiy9bqP5di-gTkYjrhW940ec',
-    onPlaceSelected: (place) => {
-      console.log('Selected Place:', place);
-      if (place.formatted_address) {
-        setInputLocation(place.formatted_address);
-        setLocation(place.formatted_address);
-      }
-      if (place.geometry && place.geometry.location) {
-        console.log('Latitude:', place.geometry.location.lat());
-        console.log('Longitude:', place.geometry.location.lng());
-        setLatitude(place.geometry.location.lat());
-        setLongitude(place.geometry.location.lng());
-      }
-    },
-    options: {
-      types: [],
-      componentRestrictions: null,
-    },
-  });
 
 
 
@@ -115,22 +91,12 @@ export default function CreateTrailPage({ trailItems, setTrailItems }) {
               />
             </div>
             <div className="mb-5">
-              <label className="mb-3 block text-base font-medium text-green-800 text-left" htmlFor="location">
-                Location
-              </label>
-              <input
-                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-green-700 focus:shadow-md"
-                type="text"
-                name="location"
-                value={inputLocation}
-                onChange={(e) => {
-                  setInputLocation(e.target.value);
-                  setLocation(e.target.value);
-                  handleChange(e);
-                }}
-                ref={materialRef}
-                autoComplete="on"
-                required
+              <GoogleSearchBar
+                location={location}
+                setLocation={setLocation}
+                handleChange={handleChange}
+                setLatitude={setLatitude}
+                setLongitude={setLongitude}
               />
             </div>
 
